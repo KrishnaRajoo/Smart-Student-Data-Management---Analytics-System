bc417b4d47eeeeef9563aa711c3065d8a29b1ca9
Krishna Rajoo <krishnarajoo06395@gmail.com>
2026-07-08 15:32:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{os.getenv('DB_USER')}:"
         f"{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}/"
+        f"{os.getenv('DB_HOST')}:"
+        f"{os.getenv('DB_PORT', '3306')}/"
         f"{os.getenv('DB_NAME')}"
     )
 
